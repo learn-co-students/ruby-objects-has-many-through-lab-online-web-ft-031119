8798aacb8d02daf67b11217ef46c6e5a5b5ecbae
Eric Storey <estorey@gmail.com>
2019-03-28 02:28:40 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,23 @@
+class Genre 
+  attr_accessor :name
+  @@all=[]
+  
+  def self.all 
+    @@all 
+  end
+  
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  
+  def songs 
+    Song.all.select{|song| song.genre==self}
+  end
+  
+  def artists
+    artists=[]
+    self.songs.each{|song| artists << song.artist}
+    artists
+  end
+end
